c44337b49e1ac0cbaafec3f0ee4611869ce6be16
Tom Dobson <dobson980@gmail.com>
2024-07-06 23:00:20 -0700

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Thomas Dobson on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
