4b6afc5add7dec8a5ffef93143fc1a18f5c36a0a
Tom Dobson <dobson980@gmail.com>
2024-07-14 00:10:07 -0700

    added readme, license, screenshots

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     let quotes = [
@@ -30,38 +31,122 @@ struct ContentView: View {
         "You are a star!",
         "Shine bright and inspire!"
     ]
-    @State private var inspiration: String
+    
+    @State private var quoteNumber: Int
+    @State private var imageNumber: Int
+    @State private var soundNumber: Int
+    @State private var soundIsOn = true
+    @State private var audioPlayer: AVAudioPlayer!
     
     init() {
-        _inspiration = State(initialValue: quotes.randomElement() ?? "You are awesome!")
+        _quoteNumber = State(initialValue: Int.random(in: 0..<quotes.count))
+        _imageNumber = State(initialValue: Int.random(in: 0...9))
+        _soundNumber = State(initialValue: Int.random(in: 0...5))
     }
     
     var body: some View {
         
-        VStack {
-            Text(inspiration)
-                .font(.title)
-                .fontWeight(.heavy)
-                .foregroundColor(.indigo)
-                .italic()
-                .padding()
-            
-            Button("Press Me") {
-                inspiration = getRandomQuote(excluding: inspiration)
+        GeometryReader { geometry in
+            ZStack {
+                Rectangle()
+                    .fill(
+                        Gradient(colors: [Color("Tan"), Color("Vermillion")])
+                    )
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Text(quotes[quoteNumber])
+                        .font(.title)
+                        .padding(1)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.indigo)
+                        .italic()
+                        .minimumScaleFactor(0.5)
+                        .multilineTextAlignment(.center)
+                        .frame(height: 150)
+                        .frame(maxWidth: .infinity)
+                        .background(Color("SkyBlue"))
+                        .cornerRadius(30)
+                        .padding()
+                        .animation(.easeInOut(duration: 0.15), value: imageNumber)
+                    
+                    Image("image\(imageNumber)")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(30)
+                        .shadow(radius: 30)
+                        .padding()
+                        .animation(.easeInOut(duration: 0.7), value: imageNumber)
+                    
+                    Spacer()
+                    
+                    Rectangle()
+                        .fill(.indigo)
+                        .frame(width: geometry.size.width * (2/5), height: 2)
+                        .cornerRadius(30)
+                    
+                    HStack {
+                        Text("Sound On:")
+                        Toggle("", isOn: $soundIsOn)
+                            .labelsHidden()
+                            .onChange(of: soundIsOn) {
+                                (audioPlayer != nil && audioPlayer.isPlaying) ? audioPlayer.stop() : nil
+                            }
+                        
+                        Spacer()
+                        
+                        Button("Press Me") {
+                            quoteNumber = generateNewRandomNumber(
+                                lowerBounds: 0,
+                                UpperBounds: quotes.count-1,
+                                currentNumber: quoteNumber
+                            )
+                            
+                            imageNumber = generateNewRandomNumber(
+                                lowerBounds: 0,
+                                UpperBounds: 9,
+                                currentNumber: imageNumber
+                            )
+                            
+                            let soundNumber = generateNewRandomNumber(
+                                lowerBounds: 0,
+                                UpperBounds: 5,
+                                currentNumber: soundNumber
+                            )
+                            soundIsOn ? playSound(sound: "sound\(soundNumber)") : nil
+                        }
+                        .padding()
+                        .buttonStyle(.borderedProminent)
+                    }
+                    .padding()
+                    .tint(.accentColor)
+                }
             }
-            .buttonStyle(.borderedProminent)
         }
     }
+
     
-    func getRandomQuote(excluding currentQuote: String) -> String {
-        var newQuote: String
+    func generateNewRandomNumber(lowerBounds: Int, UpperBounds: Int, currentNumber: Int) -> Int {
+        var newNumber: Int
         repeat {
-            newQuote = quotes.randomElement() ?? "You are Awesome!"
-        } while newQuote == currentQuote
-        return newQuote
+            newNumber = Int.random(in: lowerBounds...UpperBounds)
+        } while newNumber ==  currentNumber
+        return newNumber
+    }
+    
+    func playSound(sound: String) {
+        guard let soundFile = NSDataAsset(name: sound) else {
+            print("Could not read file named: \(sound)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("Error: \(error.localizedDescription)")
+        }
     }
 }
-
 
 
 #Preview {
